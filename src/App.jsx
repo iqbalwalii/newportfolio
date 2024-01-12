4ae6023f738168b128fa202f5d675e2b39883488
iqbalwalii <iqbalwali786@gmail.com>
2024-01-12 15:31:07 +0530

    added vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import ParticlesBackground from "./components/ParticlesBackground"
-import { Route, Routes } from "react-router-dom"
-import AboutPage from "./components/pages/AboutPage"
-import HomePage from "./components/pages/HomePage"
-import SocialLinks from "./components/SocialLinks"
-import GeneralFooter from "./components/GeneralFooter"
-import AllProjectsPage from "./components/pages/AllProjectsPage"
-import AllTechsPage from "./components/pages/AllTechsPage"
-
+import ParticlesBackground from "./components/ParticlesBackground";
+import { Route, Routes } from "react-router-dom";
+import AboutPage from "./components/pages/AboutPage";
+import HomePage from "./components/pages/HomePage";
+import SocialLinks from "./components/SocialLinks";
+import GeneralFooter from "./components/GeneralFooter";
+import AllProjectsPage from "./components/pages/AllProjectsPage";
+import AllTechsPage from "./components/pages/AllTechsPage";
+import { Analytics } from "@vercel/analytics/react";
 function App() {
-
   return (
     <>
       <ParticlesBackground />
       <SocialLinks />
-      
+      <Analytics />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        
-        <Route path="/about-me" element={<AboutPage />}/>
 
-        <Route path="/projects" element={<AllProjectsPage />}/>
+        <Route path="/about-me" element={<AboutPage />} />
 
-        <Route path="/technologies" element={<AllTechsPage />}/>
+        <Route path="/projects" element={<AllProjectsPage />} />
 
+        <Route path="/technologies" element={<AllTechsPage />} />
       </Routes>
 
       <GeneralFooter />
-        
-      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
